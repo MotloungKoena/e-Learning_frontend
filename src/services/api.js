623b2abf45d7d99bcace33b8a47b461d7cacdcfa
Koena Motloung <motloungkoena@gmail.com>
2026-03-20 10:29:57 +0200

    cleaned api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,5 @@
-/*import axios from 'axios';
-
-// Use your backend URL
-const API_URL = 'http://localhost:8080/api';
-
-const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-// Add token to requests if it exists
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-export default api;*/
-
 import axios from 'axios';
 
-// Use relative URL - this will be proxied to your backend
 const API_URL = '/api';
 
 const api = axios.create({
@@ -36,7 +9,6 @@ const api = axios.create({
   },
 });
 
-// Add token to requests if it exists
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
@@ -49,7 +21,8 @@ api.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
-// Add response interceptor for debugging
+
+// Added response interceptor for debugging
 api.interceptors.response.use(
   (response) => response,
   (error) => {
